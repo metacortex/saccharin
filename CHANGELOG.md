f2acd9b63ad1e4755cdc12e57592de76873caa8c
metacortex <9ruvie@gmail.com>
2017-04-27 06:57:19 +0900

    Add log helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,12 +6,21 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
-### Added
+<!-- ### Added
 ### Changed
 ### Deprecated
 ### Removed
 ### Fixed
-### Security
+### Security -->
+
+## [0.2.0] - 2017-4-27
+
+### Added
+- Add `_LOG`, `_LOG_PROPS` helpers
+
+### Changed
+- Support crystal 0.22.0
+
 
 ## 0.1.0 - 2017-03-31
 
@@ -19,4 +28,5 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 - Add `present?`, `empty?` methods
 - Add `dig`, `dig?` to `JSON::Any`, `YAML::Any`
 
-[Unreleased]: https://github.com/metacortex/saccharin/compare/v0.1.0...HEAD
+[Unreleased]: https://github.com/metacortex/saccharin/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/metacortex/saccharin/compare/v0.1.0...v0.2.0
---
+++ src/saccharin/log.cr
@@ -0,0 +1,49 @@
+require "file_utils"
+
+module Saccharin
+  module Logger
+    class LogFile
+      begin
+        FileUtils.mkdir("log")
+      rescue ex : Errno
+      end
+
+      INSTANCE = File.new("log/#{ENV["APP_ENV"]}.log", "a")
+
+      def self.instance
+        INSTANCE
+      end
+    end
+
+    def _log(str)
+      # STDIO
+      puts str
+
+      # file에 작성
+      f = LogFile.instance
+      f.puts str
+      f.flush
+
+      true
+    end
+  end
+end
+
+def _LOG(str)
+  return if ENV["ENV"]? == "test"
+  _log(str)
+end
+
+def _LOG_PROPS(props, padding : Int32 = 0)
+  return if ENV["ENV"]? == "test"
+
+  prefix = ""
+
+  if padding > 0
+    prefix = "%#{padding}s" % ""
+  end
+
+  props.each do |k,v|
+    _log("#{prefix}#{k}: #{v}")
+  end
+end
