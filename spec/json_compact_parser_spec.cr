42f2b9306f535458b13a50fc84e7ae3c955820b4
metacortex <9ruvie@gmail.com>
2017-05-02 06:08:35 +0900

    add spec for complex json_compact

--- spec/json_compact_parser_spec.cr
+++ spec/json_compact_parser_spec.cr
@@ -16,4 +16,28 @@ describe Saccharin do
     r2.size.should eq(0)
   end
 
+  it "parse complex json_compact" do
+    json1 = JSON.parse [{
+      "type" => "table",
+      "columns" => ["cid", "parent_cid", "name"],
+      "rows" => [
+        ["121266001", "0", "众筹"],
+        ["120886001", "0", "公益"],
+        ["98", "0", "包装"],
+        ["120950002", "0", "天猫点券"],
+        ["120894001", "0", "淘女郎"],
+        ["50023722", "0", "隐形眼镜/护理液"],
+        ["50026555", "0", "购物提货券"],
+        ["125022006", "0", "全球购官网直购（专用）"],
+        ["125102006", "0", "到家业务"],
+        ["125406001", "0", "享淘卡"],
+        ["126040001", "0", "橙运"]
+      ]
+    }].to_json
+
+    r1 = Saccharin.parse_json_compact(json1[0])
+    r1.size.should eq(11)
+    r1[0]["parent_cid"].should eq("0")
+  end
+
 end
