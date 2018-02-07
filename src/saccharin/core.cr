04a5b2b3417fbba54dda386f95be2e94ba180b6b
metacortex <9ruvie@gmail.com>
2018-02-07 20:34:44 +0900

    Time::Span

--- src/saccharin/core.cr
+++ src/saccharin/core.cr
@@ -2,20 +2,20 @@ require "json"
 require "yaml"
 
 # == Core extensions
-# 
+#
 # ++present?++, ++blank?++
-# 
+#
 module Saccharin
   module PresentAndBlankBySize
     def present?
       size > 0
     end
-    
+
     def blank?
       size == 0
     end
   end
-  
+
   module PresentAndBlankForAny
     def present?
       begin
@@ -24,7 +24,7 @@ module Saccharin
         raw.present?
       end
     end
-    
+
     def blank?
       begin
         size == 0
@@ -33,22 +33,22 @@ module Saccharin
       end
     end
   end
-  
+
   module PresentAndBlankForValue
     def present?
       true
     end
-    
+
     def blank?
       false
     end
   end
-  
+
   module PresentAndBlankForNil
     def present?
       false
     end
-    
+
     def blank?
       true
     end
@@ -87,6 +87,10 @@ struct Time
   include Saccharin::PresentAndBlankForValue
 end
 
+struct Time::Span
+  include Saccharin::PresentAndBlankForValue
+end
+
 struct Nil
   include Saccharin::PresentAndBlankForNil
 end
@@ -95,7 +99,7 @@ struct Bool
   def present?
     self
   end
-  
+
   def blank?
     !self
   end
